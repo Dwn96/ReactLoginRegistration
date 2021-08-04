f22ec660394b887fe841e63b3bc09e0fc3660f8a
Dan Njeru <wambu43@gmail.com>
2021-08-04 23:20:53 +0300

    Display image response from db

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React,{useState} from 'react'
+import axios from 'axios';
 
 const Profile = () => {
+    
+    const [binaryData,setBinaryData] = useState('');
+
+    var imageName = localStorage.getItem('imageName');
+    const url = 'http://localhost:8080/api/v1/image/'
+
+    const[image,setImage] = useState('');
+
+    axios.get(url+imageName)
+        .then((response)=>{
+           setBinaryData(response.data.data);
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+
+
+
     return (
         <div>
             <h1>Hello, {}</h1>
-            <img src="" alt="Italian Trulli"></img>
+            <img src={`data:image/jpeg;base64,${binaryData}`} />
 
             
         </div>
